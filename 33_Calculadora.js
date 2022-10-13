69062bc20ccbb4f5aaf9e4aa6edcc91ac05bad05
Italo-Guedes <84642536+Italo-Guedes@users.noreply.github.com>
2022-10-13 13:23:03 -0300

    Add files via upload

---
+++ 33_Calculadora.js
@@ -0,0 +1,34 @@
+
+let prompt = require('prompt-sync')();
+
+let num1 = parseFloat(prompt("Informe o primeiro número: "))
+let num2 = parseFloat(prompt("Informe o segundo número: "))
+
+console.log("Escolha a operação:\n")
+
+console.log("Digite + para Adição.")
+console.log("Digite - para Subtração.")
+console.log("Digite * para Multiplicação.")
+console.log("Digite / para Divisão.")
+
+let operacao = prompt()
+
+switch (operacao) {
+  case '+':
+    console.log(num1+num2)
+    break
+  case '-':
+    console.log(num1-num2)
+    break
+  case '*':
+    console.log(num1*num2)
+    break
+  case '/':
+    if (num2 == 0) {
+      console.log("Impossível dividir por zero.")
+      break }
+    else if((num1%num2)!=0){
+      	console.log("Resultado:",parseInt(num1/num2), "Resto:", num1%num2) }
+    else
+        console.log(num1/num2)
+  }
